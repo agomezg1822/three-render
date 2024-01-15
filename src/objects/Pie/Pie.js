8847f9a3d22cff7eaab2e13207d3d63fdb4a1ae2
andres <andres_felipe1822@hotmail.com>
2024-01-14 20:48:01 -0500

    v0.0.5

--- src/objects/Pie/Pie.js
+++ src/objects/Pie/Pie.js
@@ -1,4 +1,4 @@
-import { Group } from 'three';
+import { Group, MeshStandardMaterial } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import MODEL from './Pie.glb';
 import { Color } from '../../three.module';
@@ -16,29 +16,43 @@ export default class Pie extends Group {
       this.scale.set(10, 10, 10);
       this.position.set(0,0,0);
     
+      for (let i = 1; i < 100; i++) {
+        const asignacion = i.toString().padStart(2, '0');
+        const objeto_existente = gltf.scene.getObjectByName(`i${asignacion}`);
+        objeto_existente.scale.y = i;
 
-      for (let i = 1; i <= 5; i++) {
-        let asignacion = i.toString().padStart(2, '0');
-        let objeto_existente = bpy.data.objects[asignacion];
-        
-        if (objeto_existente) {
-            objeto_existente.scale.z = valor_z[i - 1];
-        }
+       // Crear un nuevo material para cada objeto
+       const nuevoMaterial = objeto_existente.material.clone();
+
+       // Asignar color según el rango de la escala en y
+       if (10 >= objeto_existente.scale.y >= 20) {
+         // Verde
+         objeto_existente.material.color.setHex(0x00ff00);
+       } 
+       else if (i >= 21 && i <= 40) {
+         // azul
+         objeto_existente.material.color.setHex(0x0000ff);
+       }
+       else if (i>= 41 && i <= 60) {
+        // amarillo
+        objeto_existente.material.color.setHex(0xffff00);
+       }
+       else if (i >= 61 && i<= 80){
+        //naranja
+        objeto_existente.material.color.setHex(0xffa500);
+       }
+       else if (i >= 81 && i<= 99){
+        //rojo
+        objeto_existente.material.color.setHex(0xff0000);
+       }
+       else {
+        //Blanco
+        objeto_existente.material.color.setHex(0xffffff);
+       }
+      //  // Asignar el nuevo material al objeto
+        objeto_existente.material = nuevoMaterial;
       };
-    
-      const objeto1 = gltf.scene.getObjectByName('01');
-      const objeto5 = gltf.scene.getObjectByName('05');
-
-      if (objeto1){
-        var valorEnY = 50;
-        var total = valorEnY * 2;
-        objeto1.scale.y = total;
-      };
-
-      if (objeto5){
-        objeto5.scale.y= 30*2;
-      }
-
     });
-  }
-}
+  };
+};
+
