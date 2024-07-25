3a8dacc901b3270d48353676b89f9bf187ccb94e
andres <andres_felipe1822@hotmail.com>
2024-07-24 20:47:32 -0500

    build webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,53 @@
+// const path = require("path");
+// const pkg = require("./package.json");
+// const CopyPlugin = require("copy-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const buildPath = "./docs/";
+
+// const base =
+//   process.env.BUILD === "production"
+//     ? "https://agomezg1822.github.io/visualizacion-3d/"
+//     : false;
+
+// module.exports = {
+//   entry: ["./src/entry.js"],
+//   output: {
+//     path: path.join(__dirname, buildPath),
+//     filename: "[name].[hash].js",
+//   },
+//   mode: "development",
+//   target: "web",
+//   devtool: "source-map",
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         use: "babel-loader",
+//         exclude: path.resolve(__dirname, "./node_modules/"),
+//       },
+//       {
+//         test: /\.(jpe?g|png|gif|svg|tga|glb|babylon|mtl|pcb|pcd|prwm|obj|mat|mp3|ogg)$/i,
+//         use: "file-loader",
+//         exclude: path.resolve(__dirname, "./node_modules/"),
+//       },
+//       {
+//         test: /\.(xlsx)$/i,
+//         use: "file-loader",
+//         exclude: path.resolve(__dirname, "./node_modules/"),
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       title: "Visualizacion 3D",
+//       base,
+//     }),
+//     new CopyPlugin({
+//       patterns: [{ from: "src/public", to: "" }],
+//     }),
+//   ],
+// };
+
 const path = require("path");
 const pkg = require("./package.json");
 const CopyPlugin = require("copy-webpack-plugin");
@@ -7,7 +57,7 @@ const buildPath = "./docs/";
 const base =
   process.env.BUILD === "production"
     ? "https://agomezg1822.github.io/visualizacion-3d/"
-    : false;
+    : "/";
 
 module.exports = {
   entry: ["./src/entry.js"],
@@ -41,9 +91,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Visualizacion 3D",
       base,
+      template: "./src/index.html", // Usa una plantilla HTML específica si la tienes
     }),
     new CopyPlugin({
-      patterns: [{ from: "src/public", to: "" }],
+      patterns: [
+        { from: "src/public", to: "" },
+        { from: "src/styles.css", to: "styles.css" }, // Copia styles.css al directorio de salida
+        { from: "src/datos.xlsx", to: "datos.xlsx" }, // Copia datos.xlsx al directorio de salida
+      ],
     }),
   ],
 };
