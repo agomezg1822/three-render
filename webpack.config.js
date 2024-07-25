8111dd50a0672bcbe99b7414220b8f5fef815182
andres <andres_felipe1822@hotmail.com>
2024-07-24 21:37:09 -0500

    prueba datos xlsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,115 +1,46 @@
-// const path = require("path");
-// const pkg = require("./package.json");
-// const CopyPlugin = require("copy-webpack-plugin");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const buildPath = "./docs/";
-
-// const base =
-//   process.env.BUILD === "production"
-//     ? "https://agomezg1822.github.io/visualizacion-3d/"
-//     : false;
-
-// module.exports = {
-//   entry: ["./src/entry.js"],
-//   output: {
-//     path: path.join(__dirname, buildPath),
-//     filename: "[name].[hash].js",
-//   },
-//   mode: "development",
-//   target: "web",
-//   devtool: "source-map",
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         use: "babel-loader",
-//         exclude: path.resolve(__dirname, "./node_modules/"),
-//       },
-//       {
-//         test: /\.(jpe?g|png|gif|svg|tga|glb|babylon|mtl|pcb|pcd|prwm|obj|mat|mp3|ogg)$/i,
-//         use: "file-loader",
-//         exclude: path.resolve(__dirname, "./node_modules/"),
-//       },
-//       {
-//         test: /\.(xlsx)$/i,
-//         use: "file-loader",
-//         exclude: path.resolve(__dirname, "./node_modules/"),
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       title: "Visualizacion 3D",
-//       base,
-//       templateContent: ({ htmlWebpackPlugin }) => `
-//         <!doctype html>
-//         <html>
-//           <head>
-//             <meta charset="utf-8">
-//             <title>${htmlWebpackPlugin.options.title}</title>
-//             <base href="${htmlWebpackPlugin.options.base}">
-//             <meta name="viewport" content="width=device-width, initial-scale=1">
-//             <link rel="stylesheet" href="styles.css"> <!-- Ruta relativa sin barra inclinada inicial -->
-//           </head>
-//           <body>
-//             <!-- Los scripts se inyectarán aquí -->
-//           </body>
-//         </html>`,
-//     }),
-//     new CopyPlugin({
-//       patterns: [
-//         { from: "src/public", to: "" },
-//         { from: "src/public/styles.css", to: "styles.css" }, // Copia styles.css al directorio de salida
-//         { from: "src/public/datos.xlsx", to: "datos.xlsx" }, // Copia datos.xlsx al directorio de salida
-//       ],
-//     }),
-//   ],
-// };
-
 const path = require("path");
+const pkg = require("./package.json");
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 const buildPath = "./docs/";
-const isProduction = process.env.BUILD === "production";
-const base = isProduction
-  ? "https://agomezg1822.github.io/visualizacion-3d/"
-  : "/";
+
+const base =
+  process.env.BUILD === "production"
+    ? "https://agomezg1822.github.io/visualizacion-3d/"
+    : false;
 
 module.exports = {
   entry: ["./src/entry.js"],
   output: {
     path: path.join(__dirname, buildPath),
     filename: "[name].[hash].js",
-    publicPath: isProduction ? "/visualizacion-3d/" : "/", // Public path for assets
   },
-  mode: isProduction ? "production" : "development",
+  mode: "development",
   target: "web",
-  devtool: isProduction ? false : "source-map",
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.js$/,
         use: "babel-loader",
-        exclude: /node_modules/,
+        exclude: path.resolve(__dirname, "./node_modules/"),
       },
       {
         test: /\.(jpe?g|png|gif|svg|tga|glb|babylon|mtl|pcb|pcd|prwm|obj|mat|mp3|ogg)$/i,
         use: "file-loader",
-        exclude: /node_modules/,
+        exclude: path.resolve(__dirname, "./node_modules/"),
       },
       {
         test: /\.(xlsx)$/i,
         use: "file-loader",
-        exclude: /node_modules/,
+        exclude: path.resolve(__dirname, "./node_modules/"),
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Visualizacion 3D",
-      base: isProduction ? base : "/",
-      inject: "body",
+      base,
       templateContent: ({ htmlWebpackPlugin }) => `
         <!doctype html>
         <html>
@@ -139,8 +70,7 @@ module.exports = {
                 .catch(error => console.error('Error al cargar el archivo datos.xlsx:', error));
             </script>
           </body>
-        </html>
-      `,
+        </html>`,
     }),
     new CopyPlugin({
       patterns: [
